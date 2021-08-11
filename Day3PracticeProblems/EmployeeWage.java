a5e8a8b07a1f96d980428149da0711ec88737eca
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-08-11 18:42:01 +0530

    UC6

--- Day3PracticeProblems/EmployeeWage.java
+++ Day3PracticeProblems/EmployeeWage.java
@@ -1,12 +1,14 @@
 
 public class EmployeeWage {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		   int isFullTime=1;
 		   int isPartTime=2;
 		   int isAbsent=0;
 		   int wageperhour=20;
-		   int totalhours;
+		   int hours;
+		   int totalhours=0;
 		   int wage=0;
 		   int sumofsalary=0;
 		   for (int i=0; i < 20; i++)
@@ -16,26 +18,34 @@ public class EmployeeWage {
 	       switch(value)
 	       {
 	       case 1:
-	    	   totalhours=16;
-	        	wage=wageperhour*totalhours;
+	    	   hours=16;
+	        	wage=wageperhour*hours;
 	        	sumofsalary=sumofsalary+wage;
+	        	totalhours=totalhours+hours;
 	        	break;
 	       case 2:
-	    	   totalhours=8;
-	        	wage=wageperhour*totalhours;
+	    	   hours=8;
+	        	wage=wageperhour*hours;
 	        	sumofsalary=sumofsalary+wage;
+	        	totalhours=totalhours+hours;
 	        	break;
 	       case 0:
 	    	    sumofsalary=sumofsalary+wage;
 	        	break;
 	       }
+	       if(totalhours>=100)
+	    	   break;
 	    }
+		   System.out.println("Total Hours of Month:" + totalhours);
 		   System.out.println("Total Monthly Salary (20 days) which include parttime,fulltime and absence is:" + sumofsalary);
 	}
 
 
 }
+
 /*Output:
- o1:Total Monthly Salary (20 days) which include parttime,fulltime and absence is:5120
- o2:Total Monthly Salary (20 days) which include parttime,fulltime and absence is:5280
- */
+o1:Total Hours of Month:104
+Total Monthly Salary (20 days) which include parttime,fulltime and absence is:2720
+o2:Total Hours of Month:104
+Total Monthly Salary (20 days) which include parttime,fulltime and absence is:3520
+*/
