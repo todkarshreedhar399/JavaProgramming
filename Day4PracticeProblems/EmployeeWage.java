265d2ae38eb70744a48de27c8bff5c5b04b07c81
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-08-13 13:06:21 +0530

    EmpWageDay4UC2

--- Day4PracticeProblems/EmployeeWage.java
+++ Day4PracticeProblems/EmployeeWage.java
@@ -1,22 +1,35 @@
 
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int isPresent=1;
-        double attendance=Math.floor(Math.random()*10)%2;
-        if (attendance == isPresent)
-        {
-        	System.out.println("Employee is Present");
-        }
-        else 
-        {
-        	System.out.println("Employee is Absent");
-        }
+		   int isPresent=1;
+		   int wageperhour=20;
+		   int totalhours=8;
+		   int wage=0;
+	        double attendance=Math.floor(Math.random()*10)%2;
+	        if (attendance == isPresent)
+	        {
+	        	System.out.println("Employee is Present");
+	        	wage=wageperhour*totalhours;
+	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	
+	        }
+	        else 
+	        {
+	        	System.out.println("Employee is Absent");
+	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	
+	        }
 	}
 
 }
 /*Output:
- *Output 1: Employee is Absent
- *Output 2: Employee is Present
- */
+ Output1: 
+  Employee is Present
+  Full Time Day Wage:160
+ Output 2:
+  Employee is Absent
+  Full Time Day Wage:0   
+*/
