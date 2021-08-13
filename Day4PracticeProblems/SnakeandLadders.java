df81c3edf8b9b66abb12a8d5f9ae2be22db55610
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-08-13 11:38:48 +0530

    UC1

---
+++ Day4PracticeProblems/SnakeandLadders.java
@@ -0,0 +1,15 @@
+
+public class SnakeandLadders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int position1=0;
+        System.out.println("Player1 is at "+ position1);
+	}
+
+}
+/*Output:
+
+Player1 is at 0
+
+*/
