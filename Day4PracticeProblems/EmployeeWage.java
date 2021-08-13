0b97562da123b5b161c04f30e52200e2276ec92b
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-08-13 13:09:06 +0530

    EmpWageDay4UC3

--- Day4PracticeProblems/EmployeeWage.java
+++ Day4PracticeProblems/EmployeeWage.java
@@ -1,35 +1,48 @@
 
-
 public class EmployeeWage {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		   int isPresent=1;
+		   int isFullTime=1;
+		   int isPartTime=2;
+		   int isAbsent=0;
 		   int wageperhour=20;
-		   int totalhours=8;
+		   int totalhours;
 		   int wage=0;
-	        double attendance=Math.floor(Math.random()*10)%2;
-	        if (attendance == isPresent)
+	        double attendance=Math.floor(Math.random()*10)%3;
+	        if (attendance == isFullTime)
 	        {
-	        	System.out.println("Employee is Present");
+	        	totalhours=16;
+	        	System.out.println("Employee is Full Time");
 	        	wage=wageperhour*totalhours;
 	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	
+	        }
+	        else if (attendance == isPartTime)
+	        {
+	        	totalhours=8;
+	        	System.out.println("Employee is Part Time");
+	        	wage=wageperhour*totalhours;
+	        	System.out.println("Part Time Day Wage:"+ wage);
 	        	
 	        }
 	        else 
 	        {
 	        	System.out.println("Employee is Absent");
-	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	System.out.println("Day Wage:"+ wage);
 	        	
 	        }
 	}
 
 }
 /*Output:
- Output1: 
-  Employee is Present
-  Full Time Day Wage:160
- Output 2:
-  Employee is Absent
-  Full Time Day Wage:0   
-*/
+ * output1:
+ Employee is Full Time
+ Full Time Day Wage:320
+ * output2:
+ Employee is Part Time
+ Part Time Day Wage:160
+ * output3:
+ Employee is Absent
+ Day Wage:0
+ */
